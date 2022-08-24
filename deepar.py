fb85a64400ef3d4bad71fe4f31fa773428efd56d
Rukmangadh Sai Myana <rukman.sai@gmail.com>
2022-08-24 12:13:39 -0400

    Fix formatiing

--- deepar.py
+++ deepar.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python 3.6
-#-*-coding:utf-8-*-
+# -*-coding:utf-8-*-
 
 '''
 DeepAR Model (Pytorch Implementation)
@@ -7,9 +7,9 @@ Paper Link: https://arxiv.org/abs/1704.04110
 Author: Jing Wang (jingw2@foxmail.com)
 '''
 
-import torch 
+import torch
 from torch import nn
-import torch.nn.functional as F 
+import torch.nn.functional as F
 from torch.optim import Adam
 
 import numpy as np
@@ -24,6 +24,7 @@ import util
 from datetime import date
 import argparse
 from progressbar import *
+
 
 class Gaussian(nn.Module):
 
@@ -41,13 +42,14 @@ class Gaussian(nn.Module):
         # initialize weights
         # nn.init.xavier_uniform_(self.mu_layer.weight)
         # nn.init.xavier_uniform_(self.sigma_layer.weight)
-    
+
     def forward(self, h):
         _, hidden_size = h.size()
         sigma_t = torch.log(1 + torch.exp(self.sigma_layer(h))) + 1e-6
         sigma_t = sigma_t.squeeze(0)
         mu_t = self.mu_layer(h).squeeze(0)
         return mu_t, sigma_t
+
 
 class NegativeBinomial(nn.Module):
 
@@ -61,12 +63,13 @@ class NegativeBinomial(nn.Module):
         super(NegativeBinomial, self).__init__()
         self.mu_layer = nn.Linear(input_size, output_size)
         self.sigma_layer = nn.Linear(input_size, output_size)
-    
+
     def forward(self, h):
         _, hidden_size = h.size()
         alpha_t = torch.log(1 + torch.exp(self.sigma_layer(h))) + 1e-6
         mu_t = torch.log(1 + torch.exp(self.mu_layer(h)))
         return mu_t, alpha_t
+
 
 def gaussian_sample(mu, sigma):
     '''
@@ -85,6 +88,7 @@ def gaussian_sample(mu, sigma):
     gaussian = torch.distributions.normal.Normal(mu, sigma)
     ypred = gaussian.sample(mu.size())
     return ypred
+
 
 def negative_binomial_sample(mu, alpha):
     '''
@@ -105,21 +109,23 @@ def negative_binomial_sample(mu, alpha):
     ypred = mu + torch.randn(mu.size()) * torch.sqrt(var)
     return ypred
 
+
 class DeepAR(nn.Module):
 
-    def __init__(self, input_size, embedding_size, hidden_size, num_layers, lr=1e-3, likelihood="g"):
+    def __init__(self, input_size, embedding_size,
+                 hidden_size, num_layers, lr=1e-3, likelihood="g"):
         super(DeepAR, self).__init__()
 
         # network
         self.input_embed = nn.Linear(1, embedding_size)
-        self.encoder = nn.LSTM(embedding_size+input_size, hidden_size, \
-                num_layers, bias=True, batch_first=True)
+        self.encoder = nn.LSTM(embedding_size+input_size, hidden_size,
+                               num_layers, bias=True, batch_first=True)
         if likelihood == "g":
             self.likelihood_layer = Gaussian(hidden_size, 1)
         elif likelihood == "nb":
             self.likelihood_layer = NegativeBinomial(hidden_size, 1)
         self.likelihood = likelihood
-    
+
     def forward(self, X, y, Xf):
         '''
         Args:
@@ -149,10 +155,12 @@ class DeepAR(nn.Module):
             else:
                 yembed = self.input_embed(ynext).view(num_ts, -1)
                 x = Xf[:, s-seq_len, :].view(num_ts, -1)
-            x = torch.cat([x, yembed], dim=1) # num_ts, num_features + embedding
+            # num_ts, num_features + embedding
+            x = torch.cat([x, yembed], dim=1)
             inp = x.unsqueeze(1)
             if h is None and c is None:
-                out, (h, c) = self.encoder(inp) # h size (num_layers, num_ts, hidden_size)
+                # h size (num_layers, num_ts, hidden_size)
+                out, (h, c) = self.encoder(inp)
             else:
                 out, (h, c) = self.encoder(inp, (h, c))
             hs = h[-1, :, :]
@@ -173,7 +181,8 @@ class DeepAR(nn.Module):
         mu = torch.cat(mus, dim=1).view(num_ts, -1)
         sigma = torch.cat(sigmas, dim=1).view(num_ts, -1)
         return ypred, mu, sigma
-    
+
+
 def batch_generator(X, y, num_obs_to_train, seq_len, batch_size):
     '''
     Args:
@@ -194,11 +203,12 @@ def batch_generator(X, y, num_obs_to_train, seq_len, batch_size):
     yf = y[batch, t:t+seq_len]
     return X_train_batch, y_train_batch, Xf, yf
 
+
 def train(
-    X, 
+    X,
     y,
     args
-    ):
+):
     '''
     Args:
     - X (array like): shape (num_samples, num_features, num_periods)
@@ -211,11 +221,11 @@ def train(
     - num_results_to_sample (int): how many samples in test phase as prediction
     '''
     num_ts, num_periods, num_features = X.shape
-    model = DeepAR(num_features, args.embedding_size, 
-        args.hidden_size, args.n_layers, args.lr, args.likelihood)
+    model = DeepAR(num_features, args.embedding_size,
+                   args.hidden_size, args.n_layers, args.lr, args.likelihood)
     optimizer = Adam(model.parameters(), lr=args.lr)
     random.seed(2)
-    # select sku with most top n quantities 
+    # select sku with most top n quantities
     Xtr, ytr, Xte, yte = util.train_test_split(X, y)
     losses = []
     cnt = 0
@@ -237,16 +247,17 @@ def train(
     for epoch in progress(range(args.num_epoches)):
         # print("Epoch {} starts...".format(epoch))
         for step in range(args.step_per_epoch):
-            Xtrain, ytrain, Xf, yf = batch_generator(Xtr, ytr, num_obs_to_train, seq_len, args.batch_size)
+            Xtrain, ytrain, Xf, yf = batch_generator(
+                Xtr, ytr, num_obs_to_train, seq_len, args.batch_size)
             Xtrain_tensor = torch.from_numpy(Xtrain).float()
             ytrain_tensor = torch.from_numpy(ytrain).float()
-            Xf = torch.from_numpy(Xf).float()  
+            Xf = torch.from_numpy(Xf).float()
             yf = torch.from_numpy(yf).float()
             ypred, mu, sigma = model(Xtrain_tensor, ytrain_tensor, Xf)
             # ypred_rho = ypred
             # e = ypred_rho - yf
             # loss = torch.max(rho * e, (rho - 1) * e).mean()
-            ## gaussian loss
+            # gaussian loss
             ytrain_tensor = torch.cat([ytrain_tensor, yf], dim=1)
             if args.likelihood == "g":
                 loss = util.gaussian_likelihood_loss(ytrain_tensor, mu, sigma)
@@ -257,11 +268,12 @@ def train(
             loss.backward()
             optimizer.step()
             cnt += 1
-    
-    # test 
+
+    # test
     mape_list = []
     # select skus with most top K
-    X_test = Xte[:, -seq_len-num_obs_to_train:-seq_len, :].reshape((num_ts, -1, num_features))
+    X_test = Xte[:, -seq_len-num_obs_to_train:-
+                 seq_len, :].reshape((num_ts, -1, num_features))
     Xf_test = Xte[:, -seq_len:, :].reshape((num_ts, -1, num_features))
     y_test = yte[:, -seq_len-num_obs_to_train:-seq_len].reshape((num_ts, -1))
     yf_test = yte[:, -seq_len:].reshape((num_ts, -1))
@@ -275,33 +287,37 @@ def train(
         if yscaler is not None:
             y_pred = yscaler.inverse_transform(y_pred)
         result.append(y_pred.reshape((-1, 1)))
-    
+
     result = np.concatenate(result, axis=1)
     p50 = np.quantile(result, 0.5, axis=1)
     p90 = np.quantile(result, 0.9, axis=1)
     p10 = np.quantile(result, 0.1, axis=1)
-    
+
     mape = util.MAPE(yf_test, p50)
     print("P50 MAPE: {}".format(mape))
     mape_list.append(mape)
 
     if args.show_plot:
         plt.figure(1, figsize=(20, 5))
-        plt.plot([k + seq_len + num_obs_to_train - seq_len \
-            for k in range(seq_len)], p50, "r-")
-        plt.fill_between(x=[k + seq_len + num_obs_to_train - seq_len for k in range(seq_len)], \
-            y1=p10, y2=p90, alpha=0.5)
+        plt.plot([k + seq_len + num_obs_to_train - seq_len
+                  for k in range(seq_len)], p50, "r-")
+        plt.fill_between(x=[k + seq_len + num_obs_to_train - seq_len
+                            for k in range(seq_len)],
+                         y1=p10, y2=p90, alpha=0.5)
         plt.title('Prediction uncertainty')
         yplot = yte[-1, -seq_len-num_obs_to_train:]
         plt.plot(range(len(yplot)), yplot, "k-")
-        plt.legend(["P50 forecast", "true", "P10-P90 quantile"], loc="upper left")
+        plt.legend(["P50 forecast", "true", "P10-P90 quantile"],
+                   loc="upper left")
         ymin, ymax = plt.ylim()
-        plt.vlines(seq_len + num_obs_to_train - seq_len, ymin, ymax, color="blue", linestyles="dashed", linewidth=2)
+        plt.vlines(seq_len + num_obs_to_train - seq_len, ymin, ymax,
+                   color="blue", linestyles="dashed", linewidth=2)
         plt.ylim(ymin, ymax)
         plt.xlabel("Periods")
         plt.ylabel("Y")
         plt.show()
     return losses, mape_list
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -314,7 +330,8 @@ if __name__ == "__main__":
     parser.add_argument("--likelihood", "-l", type=str, default="g")
     parser.add_argument("--seq_len", "-sl", type=int, default=7)
     parser.add_argument("--num_obs_to_train", "-not", type=int, default=1)
-    parser.add_argument("--num_results_to_sample", "-nrs", type=int, default=10)
+    parser.add_argument("--num_results_to_sample",
+                        "-nrs", type=int, default=10)
     parser.add_argument("--show_plot", "-sp", action="store_true")
     parser.add_argument("--run_test", "-rt", action="store_true")
     parser.add_argument("--standard_scaler", "-ss", action="store_true")
@@ -327,10 +344,12 @@ if __name__ == "__main__":
 
     if args.run_test:
         data_path = util.get_data_path()
-        data = pd.read_csv(os.path.join(data_path, "LD_MT200_hour.csv"), parse_dates=["date"])
+        data = pd.read_csv(os.path.join(
+            data_path, "LD_MT200_hour.csv"), parse_dates=["date"])
         data["year"] = data["date"].apply(lambda x: x.year)
         data["day_of_week"] = data["date"].apply(lambda x: x.dayofweek)
-        data = data.loc[(data["date"] >= date(2014, 1, 1)) & (data["date"] <= date(2014, 3, 1))]
+        data = data.loc[(data["date"] >= date(2014, 1, 1)) &
+                        (data["date"] <= date(2014, 3, 1))]
 
         features = ["hour", "day_of_week"]
         # hours = pd.get_dummies(data["hour"])
