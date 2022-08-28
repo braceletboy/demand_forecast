a7174ca177fe221eff11a565ae76067b65abc0e3
Rukmangadh Sai Myana <rukman.sai@gmail.com>
2022-08-27 20:00:43 -0400

    Fix bugs

--- util.py
+++ util.py
@@ -4,6 +4,7 @@
 '''
 Utility functions
 '''
+import torch
 import numpy as np
 import os
 
@@ -120,3 +121,44 @@ class LogScaler:
 
     def transform(self, y):
         return np.log1p(y)
+
+
+def gaussian_likelihood_loss(z, mu, sigma):
+    '''
+    Gaussian Liklihood Loss
+    Args:
+    z (tensor): true observations, shape (num_ts, num_periods)
+    mu (tensor): mean, shape (num_ts, num_periods)
+    sigma (tensor): standard deviation, shape (num_ts, num_periods)
+
+    likelihood: 
+    (2 pi sigma^2)^(-1/2) exp(-(z - mu)^2 / (2 sigma^2))
+
+    log likelihood:
+    -1/2 * (log (2 pi) + 2 * log (sigma)) - (z - mu)^2 / (2 sigma^2)
+    '''
+    negative_likelihood = torch.log(
+        sigma + 1) + (z - mu) ** 2 / (2 * sigma ** 2) + 6
+    return negative_likelihood.mean()
+
+
+def negative_binomial_loss(ytrue, mu, alpha):
+    '''
+    Negative Binomial Sample
+    Args:
+    ytrue (array like)
+    mu (array like)
+    alpha (array like)
+
+    maximuze log l_{nb} = log Gamma(z + 1/alpha) - log Gamma(z + 1) - log Gamma(1 / alpha)
+                - 1 / alpha * log (1 + alpha * mu) + z * log (alpha * mu / (1 + alpha * mu))
+
+    minimize loss = - log l_{nb}
+
+    Note: torch.lgamma: log Gamma function
+    '''
+    batch_size, seq_len = ytrue.size()
+    likelihood = torch.lgamma(ytrue + 1. / alpha) - torch.lgamma(ytrue + 1) - torch.lgamma(1. / alpha) \
+        - 1. / alpha * torch.log(1 + alpha * mu) \
+        + ytrue * torch.log(alpha * mu / (1 + alpha * mu))
+    return - likelihood.mean()
